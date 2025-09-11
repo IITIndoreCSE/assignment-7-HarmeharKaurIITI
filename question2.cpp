f32c4f8e6fb44b299a60be70ee74eb8085740bc6
HarmeharKaurIITI <me250003034@iiti.ac.in>
2025-09-11 11:40:08 +0530

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -17,16 +17,42 @@ using namespace std;
 // TODO: Define your structure here
 struct Complex {
     // your code here
+    double real;
+    double imag;
 };
 
 // TODO: Implement input processing
 void processInput(vector<Complex> &arr) {
     // your code here
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        Complex c;
+        cin>>c.real>>c.imag;
+        arr.push_back(c);
+    
+    }
 }
 
 // TODO: Implement operations
 void performOperations(vector<Complex> &arr) {
     // your code here
+    double additionReal=0;
+    double additionImag=0;
+    for(int j=0;j<arr.size();j++){
+        additionReal+=arr[j].real;
+        additionImag+=arr[j].imag;
+    }
+    double subtractionReal=arr[0].real;
+    double subtractionImag=arr[0].imag;
+    if (arr.size()!=1){
+    for (int k=1;k<arr.size();k++){
+        subtractionReal-=arr[k].real;
+        subtractionImag-=arr[k].imag;}
+    }
+    cout<<"Sum: "<<additionReal<<" + "<<additionImag<<'i'<<endl;
+    cout<<"Diff: "<<subtractionReal<<" + "<<subtractionImag<<'i'<<endl;
+        
 }
 
 // -------------------------
