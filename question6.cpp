9c0971af62b13f2c323cb7705b0c5210e023648a
HarmeharKaurIITI <me250003034@iiti.ac.in>
2025-09-13 13:01:47 +0530

    Update question6.cpp

--- question6.cpp
+++ question6.cpp
@@ -17,12 +17,27 @@ using namespace std;
 // Define your structure here
 struct Cricketer {
     // TODO: Implement struct with name, matches, and runs[]
+    string name;
+    int matches;
+    vector <int> runs;
 };
 
 // Process input function
 void processInput(vector<Cricketer> &players) {
     // TODO: Take input for number of players and their details
     // Hint: resize vector, then input name, matches, and runs
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        Cricketer c;
+        cin>>c.name>>c.matches;
+        c.runs.resize(c.matches);
+        for (int j=0;j<c.matches;j++){
+            cin>>c.runs[j];
+        }
+        players.push_back(c);
+    }
+    
 }
 
 // Perform operations function
@@ -31,6 +46,21 @@ void performOperations(vector<Cricketer> &players) {
     // 1. Calculate average runs for each player
     // 2. Print each player’s average
     // 3. Find and print best player with highest average
+    double largest=0;
+    int indexno=0;
+    for (int k=0;k<players.size();k++){
+        double sum=0;
+        for(int m=0;m<players[k].matches;m++){
+            sum+=players[k].runs[m];
+        }
+        double avg=sum/players[k].matches;
+        cout<<players[k].name<<" Avg: "<<avg<<endl;
+        if (avg>largest){
+            largest=avg;
+            indexno=k;
+        }
+    }
+    cout<<"Best Player: "<<players[indexno].name<<" with Avg "<<largest<<endl;
 }
 
 // -------------------------
